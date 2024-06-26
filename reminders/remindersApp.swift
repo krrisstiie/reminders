2e4ea17c32dd24418b36fde1d13fc142d4d08687
krrisstiie <kristiefongg@gmail.com>
2024-06-26 10:23:20 -0700

    Initial Commit

---
+++ reminders/remindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindersApp.swift
+//  reminders
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct remindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
